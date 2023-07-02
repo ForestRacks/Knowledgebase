cdea679148c7de1600cc2c9f2bd8e6f68b790b8f
Zinidia <zinidiapayments@gmail.com>
2023-07-02 17:28:24 -0500

    Added open graph meta

--- theme.config.tsx
+++ theme.config.tsx
@@ -7,6 +7,13 @@ const config: DocsThemeConfig = {
       titleTemplate: '%s – ForestRacks'
     }
   },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="ForestRacks" />
+      <meta property="og:description" content="ForestRacks Help Center" />
+    </>
+  ),
   logo: <span>ForestRacks</span>,
   project: {
     link: 'https://github.com/ForestRacks/Knowledgebase',
