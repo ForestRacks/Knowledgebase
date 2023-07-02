6c28cf03320b62566281d19386241fa65551fa34
Zinidia <zinidiapayments@gmail.com>
2023-07-02 17:24:31 -0500

    Added brand name to title

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – ForestRacks'
+    }
+  },
   logo: <span>ForestRacks</span>,
   project: {
     link: 'https://github.com/ForestRacks/Knowledgebase',
