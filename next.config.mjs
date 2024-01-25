71b44d0496e441548d3d297c8aaa69344e6027d0
Zinidia <zinidiapayments@gmail.com>
2024-01-24 23:35:47 -0600

    Start migration to Nextra v3

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+import nextra from 'nextra'
+
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+  latex: true,
+})
+
+export default withNextra({
+  reactStrictMode: true
+})
